be7d5628ca1612ffc8d8ca2a1a1cfcead620afed
MasterPorti <masterportilol@gmail.com>
2022-12-05 18:00:35 -0600

    agents img  0.0.2

--- src/pages/agents.jsx
+++ src/pages/agents.jsx
@@ -1,3 +1,36 @@
+import { useEffect, useState } from "react";
+import getAgents from "../services/getAgents";
+
+import "../scss/agents.scss";
+
 export default function Agents() {
-  return <div>Agents</div>;
+  const [agents, setAgents] = useState([]);
+  const [agentsImagen, setImagen] = useState(
+    "https://media.valorant-api.com/agents/dade69b4-4f5a-8528-247b-219e5a1facd6/fullportrait.png"
+  );
+  useEffect(() => {
+    getAgents().then((uuid) => {
+      setAgents(uuid);
+    });
+  }, []);
+  return (
+    <div>
+      <ul>
+        {agents.map((value) => {
+          return (
+            <li
+              key={value.uuid}
+              onClick={() => {
+                setImagen(value.fullPortrait);
+              }}
+            >
+              {value.displayName}
+            </li>
+          );
+        })}
+      </ul>
+
+      <img src={agentsImagen} className="imgAgents" />
+    </div>
+  );
 }
---
+++ src/services/getAgents.jsx
@@ -0,0 +1,19 @@
+const apiURL = "https://valorant-api.com/v1/agents";
+
+export default function getAgents() {
+  return fetch(apiURL)
+    .then((res) => res.json())
+    .then((response) => {
+      const { data } = response;
+      const uuid = [];
+      data.map(function (value) {
+        if (value.isPlayableCharacter)
+          uuid.push({
+            uuid: value.uuid,
+            displayName: value.displayName,
+            fullPortrait: value.fullPortrait,
+          });
+      });
+      return uuid;
+    });
+}
